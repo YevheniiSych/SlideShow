857630b6404aafafbc90fec1396c08407d75e416
yevheniisych-nc <yevhenii@Evgenijs-MacBook-Air.local>
2025-09-23 23:43:03 +0300

    Added Custom Video Surface to avoid "Black blinking" when video player starts and stops

--- app/src/main/java/com/test/slideshow/ui/slide_show/view/VideoPlayer.kt
+++ app/src/main/java/com/test/slideshow/ui/slide_show/view/VideoPlayer.kt
@@ -1,6 +1,6 @@
 package com.test.slideshow.ui.slide_show.view
 
-import android.graphics.Color
+import android.view.TextureView
 import androidx.annotation.OptIn
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,9 +11,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.common.VideoSize
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.PlayerView
 
 @OptIn(UnstableApi::class)
 @Composable
@@ -38,7 +39,6 @@ fun VideoPlayer(
         exoPlayer.playWhenReady = autoPlay
 
         onDispose {
-            exoPlayer.stop()
             exoPlayer.release()
         }
     }
@@ -47,13 +47,44 @@ fun VideoPlayer(
         AndroidView(
             modifier = Modifier
                 .fillMaxSize(),
-            factory = {
-                PlayerView(context).apply {
-                    useController = false
-                    player = exoPlayer
-                    setShutterBackgroundColor(Color.TRANSPARENT)
+            factory = { context ->
+                TextureView(context).apply {
+                    exoPlayer.setVideoTextureView(this)
+
+                    exoPlayer.addListener(object : Player.Listener {
+                        override fun onVideoSizeChanged(videoSize: VideoSize) {
+                            adjustTextureView(this@apply, videoSize)
+                        }
+                    })
                 }
             }
         )
     }
+}
+
+fun adjustTextureView(textureView: TextureView, videoSize: VideoSize) {
+    val viewW = textureView.width.toFloat()
+    val viewH = textureView.height.toFloat()
+    val videoW = videoSize.width.toFloat()
+    val videoH = videoSize.height.toFloat()
+
+    if (viewW <= 0 || viewH <= 0 || videoW <= 0 || videoH <= 0) return
+
+    val viewAspect = viewW / viewH
+    val videoAspect = videoW / videoH
+
+    val scaleX: Float
+    val scaleY: Float
+
+    if (videoAspect > viewAspect) {
+        scaleX = viewW / videoW
+        scaleY = scaleX
+    } else {
+        scaleY = viewH / videoH
+        scaleX = scaleY
+    }
+
+    val matrix = android.graphics.Matrix()
+    matrix.setScale(scaleX * videoW / viewW, scaleY * videoH / viewH, viewW / 2, viewH / 2)
+    textureView.setTransform(matrix)
 }
